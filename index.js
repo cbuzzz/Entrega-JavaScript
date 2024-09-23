cf70be896717bfb022ef944272c9f9230ff3de44
cbuzzz <sebpinsan12@gmail.com>
2024-09-23 13:43:54 +0200

    final

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ function onRequestHandler() {
 }
 
 function onRequestHandler1() {
-  // Improved processing
-  const data = JSON.parse(this.response);
-  const userIdCombined = data
-  .map(post => post.userId * 2) // Multiply userId by 2
-  .filter(userId => userId >= 14) // Filter by userId >= 1
-  .reduce((total,userId) =>total+userId,0); 
-
-const postsuser = data.filter(function(post) {
-  return post.userId === 1;
+    if (this.readyState === 4 && this.status === 200) {
+    const data = JSON.parse(this.response);
+    const userIdCombined = data
+    .map(post => post.userId * 2) // Multiply userId by 2
+    .filter(userId => userId >= 14) // Filter by userId >= 14
+    .reduce((total,userId) =>total+userId,0); 
+    
+    const postsuser = data.filter(function(post) {
+     return post.userId === 1;
 });
 
 console.log(postsuser); // Posts for user 1
 console.log(userIdCombined); // User IDs multiplied by 2, bigger than 14 and added.
+    }
 }
 
 xhr.addEventListener('load', onRequestHandler);
@@ -33,12 +34,6 @@ xhr.send();
 xhr_posts.addEventListener('load', onRequestHandler1);
 xhr_posts.open('GET', API_URL + '/posts');
 xhr_posts.send();
-
-
-
-
-
-
 
 
 
